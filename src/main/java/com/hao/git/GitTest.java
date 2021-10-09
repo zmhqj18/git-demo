4cef9a1a99edfe0b281e4575d0c9615fb7b09114
qj <qj@hao.com>
2021-10-09 22:34:49 +0800

    master push commit

--- src/main/java/com/hao/git/GitTest.java
+++ src/main/java/com/hao/git/GitTest.java
@@ -7,5 +7,6 @@ public class GitTest {
         System.out.println("hello git3 hot-fix");
         System.out.println("master test");
         System.out.println(" hot-fix test");
+        System.out.println(" push test");
     }
 }
