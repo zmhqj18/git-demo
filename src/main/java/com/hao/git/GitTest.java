cd6e2ae7466ff9568bd33ce2dd81f7d12c32c82a
qj <qj@hao.com>
2021-10-09 22:15:16 +0800

    master commit2

--- src/main/java/com/hao/git/GitTest.java
+++ src/main/java/com/hao/git/GitTest.java
@@ -5,5 +5,6 @@ public class GitTest {
         System.out.println("hello git");
         System.out.println("hello git2");
         System.out.println("hello git3 hot-fix");
+        System.out.println("master test");
     }
 }
